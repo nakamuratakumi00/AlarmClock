41641ad816c51ba9c1a3f638afeb4916aef05af4
SIC_NET\nakamura-ta <nakamura-takumi@sic-net.com>
2021-05-20 17:30:44 +0900

    フォントの調整

--- AlarmClock/AlarmClock/Form1.Designer.cs
+++ AlarmClock/AlarmClock/Form1.Designer.cs
@@ -45,8 +45,9 @@ namespace AlarmClock
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.textBox1.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.textBox1.Location = new System.Drawing.Point(52, 287);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(329, 34);
@@ -61,8 +62,9 @@ namespace AlarmClock
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.label1.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.label1.Location = new System.Drawing.Point(172, 76);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(427, 80);
@@ -73,6 +75,7 @@ namespace AlarmClock
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.label2.Location = new System.Drawing.Point(427, 292);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(548, 28);
@@ -83,6 +86,7 @@ namespace AlarmClock
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.label3.Location = new System.Drawing.Point(216, 292);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(230, 28);
@@ -104,6 +108,7 @@ namespace AlarmClock
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("HGP創英角ﾎﾟｯﾌﾟ体", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
             this.label4.Location = new System.Drawing.Point(575, 330);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(245, 28);
@@ -156,7 +161,7 @@ namespace AlarmClock
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(984, 549);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
