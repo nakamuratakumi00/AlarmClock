d74c6a8cbe0913b62b62a6521c1792eb69298af2
SIC_NET\nakamura-ta <nakamura-takumi@sic-net.com>
2021-05-20 16:46:41 +0900

    目覚まし時計の機能は完成

--- AlarmClock/AlarmClock/Form1.cs
+++ AlarmClock/AlarmClock/Form1.cs
@@ -6,14 +6,29 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Media;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
+using System.Globalization;
 
 namespace AlarmClock
 {
     public partial class Form1 : Form
     {
         bool flag = false;
+        bool datecheck = false;
+        bool alarmClockCheck = false;
+
+        DateTime dNow = DateTime.Now;
+
+        string time = "";
+        string format = "MMddHHmm";
+        DateTime dateTime;
+
+        SoundPlayer sound = new SoundPlayer();
+
+        // [DllImport("winmm.dll")]
+        // extern static bool PlaySound(string s1, IntPtr i1, int i2);
         public Form1()
         {
             InitializeComponent();
@@ -23,34 +38,29 @@ namespace AlarmClock
         {
             timer1.Interval = 1000;  // タイマの間隔を 1000[ms] に設定
             timer1.Start();          // タイマを起動
+            AlarmSet();
         }
 
-        DateTime dNow = DateTime.Now;
 
-        [DllImport("winmm.dll")]
-        extern static bool PlaySound(string s1, IntPtr i1, int i2);
         private void timer1_Tick(object sender, EventArgs e)
         {
             dNow = DateTime.Now;
-            label1.Text = dNow.ToString("F");  // 現在の時刻を表示
-            //label3.Text = dNow.ToShortTimeString();
+            label1.Text = dNow.ToString("MM/dd HH:mm:ss");  // 現在の時刻を表示
             if (flag)
             {
                 if (dNow >= dateTime)
                 {
-                    PlaySound(@"C:\Users\Nakamura-ta\OneDrive\ドキュメント\SIC\中村\windows formApp\AlarmClock\アラーム.wav", IntPtr.Zero, 0);
+                    sound.SoundLocation = @"C:\Users\nakamura-ta\ソース\AlarmClock\AlarmClock\AlarmClock\アラーム.wav";
+                    sound.PlayLooping();
+                    button4.Enabled = true;
                     flag = false;
                 }
             }
         }
-
-        string time = "";
-        string format = "MMddHHmm";
-        DateTime dateTime;
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             time = textBox1.Text;
-            label2.Text = time;
+            //label2.Text = time;
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -58,15 +68,102 @@ namespace AlarmClock
         }
         private void button1_Click(object sender, EventArgs e)
         {
-            dateTime = DateTime.ParseExact(time, format, null);
+            DayCheck();
 
-            if (MessageBox.Show("アラームを" + time + "に設定しますか？", "目覚まし時計",
-                           MessageBoxButtons.YesNo,  // ボタンの一覧は MessageBoxButtons 参照
-                           MessageBoxIcon.Question) == DialogResult.Yes)   // アイコンの一覧は  MessageBoxIcon 参照)
+            if (datecheck)
             {
-                flag = true;
-                /* [はい] ボタンの処理 */
-                label3.Text = dateTime.ToString();
+                dateTime = DateTime.ParseExact(time, format, null);
+
+                if (MessageBox.Show("アラームを" + dateTime + "に設定しますか？", "目覚まし時計",
+                               MessageBoxButtons.YesNo,  // ボタンの一覧は MessageBoxButtons 参照
+                               MessageBoxIcon.Question) == DialogResult.Yes)   // アイコンの一覧は  MessageBoxIcon 参照)
+                {
+                    flag = true;
+                    /* [はい] ボタンの処理 */
+                    label3.Text = dateTime.ToString("MM/dd HH:mm:ss") + "にアラームをセットしました。";
+                    textBox1.Text = "";
+                    alarmClockCheck = false;
+                    AlarmSet();
+                    button3.Show();
+                    button4.Show();
+                    label3.Show();
+                }
+            }
+
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Hide();
+            alarmClockCheck = true;
+            AlarmSet();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("設定したアラームを変更しますか？", "アラームの変更",
+                       MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                AlarmSet();
+                sound.Stop();
+                alarmClockCheck = true;
+                AlarmSet();
+            }
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sound.Stop();
+            button4.Enabled = false;
+            AlarmSet();
+            alarmClockCheck = true;
+            AlarmSet();
+
+        }
+        void DayCheck()
+        {
+            DateTime dt;
+            var ci = CultureInfo.CurrentCulture;
+            var dts = DateTimeStyles.None;
+            if (DateTime.TryParseExact(time, format, ci, dts, out dt))
+            {
+                if (dt < dNow)
+                {
+                    MessageBox.Show("その時刻にアラームはセット出来ません。", "エラーです。",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    datecheck = false;
+                    textBox1.Text = "";
+                    return;
+                }
+                datecheck = true;
+            }
+            else
+            {
+                MessageBox.Show("正しい数値を入力してください。", "エラーです。",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                datecheck = false;
+                textBox1.Text = "";
+                return;
+            }
+        }
+        void AlarmSet()
+        {
+            if (alarmClockCheck)
+            {
+                textBox1.Show();
+                button1.Show();
+                label2.Show();
+                label4.Show();
+            }
+            else
+            {
+                textBox1.Hide();
+                button1.Hide();
+                button3.Hide();
+                button4.Hide();
+                button4.Enabled = false;
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
             }
         }
     }
